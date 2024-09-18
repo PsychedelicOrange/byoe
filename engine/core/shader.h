93a05a7c2cee15a5e29587b454ee0eaf6d242041
Parth Saravade <parthsaravade.acad@gmail.com>
2024-09-18 22:52:56 +0530

    making sure build on linux is working

--- engine/core/shader.h
+++ engine/core/shader.h
@@ -4,12 +4,12 @@
 #include <cglm/cglm.h>
 #include <cglm/struct.h>
 
-unsigned int compile_shader(const char * shaderCode, int shaderType);
+unsigned int compile_shader(char * shaderCode, int shaderType);
 
 unsigned int create_program(unsigned int vertexShader, unsigned int fragmentShader);
 
 void setUniformMat4(unsigned int shaderProgram,mat4s matrix, char* location);
 
-char* readFileToString(const char* filename);
+static char* readFileToString(const char* filename);
 
 #endif
