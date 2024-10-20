8e00c611a0d466b8a7bc4b9cf2543b54327207cf
Pikachuxxxx <phani.s2909@icloud.com>
2024-10-20 19:00:00 +0530

    SDF rendering start

---
+++ engine/render/renderer_sdf.c
@@ -0,0 +1,11 @@
+#include "renderer_sdf.h"
+
+bool renderer_sdf_init(renderer_desc desc)
+{
+    return false;
+}
+
+void renderer_sdf_destroy()
+{
+
+}
---
+++ engine/render/renderer_sdf.h
@@ -0,0 +1,15 @@
+#ifndef RENDERER_SDF_H
+#define RENDERER_SDF_H
+
+#include <stdint.h>
+
+typedef struct renderer_desc
+{
+    uint32_t width;
+    uint32_t height;
+}renderer_desc;
+
+bool renderer_sdf_init(renderer_desc desc);
+void renderer_sdf_destroy();
+
+#endif
