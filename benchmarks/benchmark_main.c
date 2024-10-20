2b084a031fa29fb2dd1a6fbbef02707502031b0f
Pikachuxxxx <phani.s2909@icloud.com>
2024-10-20 18:43:30 +0530

    added more info to benchmarks

--- benchmarks/benchmark_main.c
+++ benchmarks/benchmark_main.c
@@ -1,9 +1,16 @@
 #include "benchmark.h"
 #include "benchmark_hash_map.h"
 
+#include <engine/core/simd/platform_caps.h>
+
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
+
+    // Print cpu/os info
+    cpu_caps_print_info();
+    os_caps_print_info();
+
 
     // Benchmarks
     benchmark_hash_map();
