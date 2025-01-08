f691cc79ede78022b05546966f4d9dac41f4e8f0
psyomen <parthsaravade.acad@gmail.com>
2025-01-08 10:55:07 +0530

    added sdf docs and headers

--- sdf/README.md
+++ sdf/README.md
@@ -1,21 +1,31 @@
-# sdf format
-## Concepts
-### Sdfs
+# architecture
+`sdf_editor` will allow creation of complex sdf shapes and will save the file as .sdf (json file)
+`.sdf` sdf format will be the format for storing `sdf_editor` projects
+`sdf_parser` will parse the `.sdf` json file into `sdf_types`
+`sdf_render` will convert a `.sdf` file to a shader file that can be imported into your engine.
+# sdf format specification
+### Inspirations and reference
+* the goat - https://iquilezles.org/articles/distfunctions/
+* gltf spec
+* cgltf lib
+### Concepts
+#### Sdfs
 These are standard 3d shapes like sphere, cube, etc.. along with parameters to define their shape.
 Operations can be specified on these shapes individually.
 Each of them can have distinct materials.
-### Primitives
+#### Primitives
 These are the collection of sdfs which are rendered together in 1 draw call.
 Operations can be specified on primitives also, which will be applied after all the sdfs operations and blending(min function) are processed.
-### Mixing
+#### Mixing
 These will define which blend function to use btw two sdfs within a primitive, as well as bolean operations like union intersection, etc.
 If not specified, by default it will use a min function and union two objects.
-### Materials
-## Apendix
-### List of Operations
-### List of Shapes
-### List of Mixing Functions
-## Structure
+#### Materials
+### Apendix
+#### List of Operations, Shapes, and Mixing functions
+see [sdftypes](./sdf_parser/sdf_types.h)
+### Examples
+see [examples](./examples)
+### Demo Structure
 ```
 {
 	"primitives":[
---
+++ sdf/sdf_parser/sdftypes.h
@@ -0,0 +1,99 @@
+#ifndef SDF_TYPE_H
+#define SDF_TYPE_H
+#include <stddef.h>
+
+/* Sdf types */
+typedef enum sdf_type{
+    plane,
+    sphere,
+    box,
+    rounded_box,
+    box_frame,
+    capsule,
+    torus,
+    capped_torus,
+    link,
+    cylinder,
+    rounded_cylinder,
+    cone,
+    round_cone,
+    frustum_cone,
+    hexagonal_prism,
+    triangular_prism,
+    solid_angle,
+    triangle,
+    quad
+}sdf_type;
+
+typedef struct sdf_plane_props{
+    float normal[3];
+    float distance_from_origin;
+}sdf_plane_props;
+
+typedef struct sdf_sphere_props{
+    float radius;
+}sdf_sphere_props;
+
+typedef struct sdf_torus_props{
+    float inner_radius;
+    float outer_radius;
+}sdf_torus_props;
+
+typedef union sdf_props{
+    sdf_plane_props plane;
+    sdf_sphere_props sphere;
+    sdf_torus_props torus;
+}sdf_props;
+
+/* Operations */
+
+typedef enum sdf_operation{
+    rotate,
+    translate,
+    scale
+}sdf_operation;
+
+typedef struct sdf_scale_operation{
+    float scale[3];
+}sdf_scale_operation;
+
+typedef struct sdf_translation_operation{
+    float translation[3];
+}sdf_translation_operation;
+
+typedef union sdf_rotation_operation{
+    float quaternion[4];
+    float axis_angle[4];
+}sdf_rotation_operation;
+
+typedef union sdf_operation_props{
+    sdf_scale_operation scale;
+    sdf_rotation_operation rotation;
+    sdf_translation_operation translation;
+}sdf_operation_props;
+
+/* sdf file and primitive */
+
+typedef struct sdf{
+    char* label;   /* Null terminated */
+    sdf_type type;
+    union sdf_props sdf_props; /* sdf specific data, will be read according to sdf_type specified*/
+    sdf_operation* sdf_operations;
+    sdf_operation_props* sdf_operation_props;
+    size_t operation_count;
+}sdf;
+
+typedef struct sdf_primtive{
+    char* label;   /* Null terminated */
+    sdf* sdfs;
+    size_t sdf_count;
+
+}sdf_primitive;
+
+typedef struct sdf_file{
+    sdf_primitive* primitives;
+    size_t primitive_count;
+    
+}sdf_file;
+
+#endif 
