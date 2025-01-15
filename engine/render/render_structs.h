dbc2f21aa387f65691948f57431a66e94d5c8713
Pikachuxxxx <phani.s2909@icloud.com>
2025-01-15 23:30:50 +0530

    Added props for all primitives;

--- engine/render/render_structs.h
+++ engine/render/render_structs.h
@@ -43,12 +43,27 @@ ENUM(SDF_PrimitiveType,
     SDF_PRIM_Box,
     SDF_PRIM_RoundedBox,
     SDF_PRIM_BoxFrame,
+    SDF_PRIM_Torus,
+    SDF_PRIM_TorusCapped,
+
     SDF_PRIM_Capsule,
     SDF_PRIM_Cylinder,
-    SDF_PRIM_Planet,
-    SDF_PRIM_Spaceship,
-    SDF_PRIM_Bullet,
-    SDF_PRIM_Ghost)
+    SDF_PRIM_Ellipsoid,
+    SDF_PRIM_Quad,
+    SDF_PRIM_HexagonalPrism,
+    SDF_PRIM_TriangularPrism,
+    SDF_PRIM_Cone,
+    SDF_PRIM_ConeSection,
+    SDF_PRIM_Plane,
+    SDF_PRIM_RoundedCylinder,
+    SDF_PRIM_SolidAngle,
+    SDF_PRIM_Line,
+    SDF_PRIM_RoundedCone,
+    SDF_PRIM_VerticalCapsule,
+    SDF_PRIM_CappedCone,
+    SDF_PRIM_CappedTorus,
+    SDF_PRIM_CappedCylinder,
+    SDF_PRIM_CappedPlane)
 
 ENUM(SDF_BlendType,
     SDF_BLEND_UNION,
@@ -89,18 +104,120 @@ STRUCT(box_frame_props,
        vec3s dimensions;
        float thickness;)
 
+STRUCT(torus_props,
+       float thickness;)
+
+STRUCT(torus_capped_props,
+       float thickness;)
+
+STRUCT(capsule_props,
+       float radius;
+       float height;)
+
+STRUCT(cylinder_props,
+       float radius;
+       float height;)
+
+STRUCT(ellipsoid_props,
+       vec3s radii;)
+
+STRUCT(quad_props,
+       vec2s dimensions;)
+
+STRUCT(hexagonal_prism_props,
+       float radius;
+       float height;)
+
+STRUCT(triangular_prism_props,
+       float radius;
+       float height;)
+
+STRUCT(cone_props,
+       float radius;
+       float height;)
+
+STRUCT(cone_section_props,
+       float radiusTop;
+       float radiusBottom;
+       float height;)
+
+STRUCT(plane_props,
+       vec3s normal;
+       float distance;)
+
+STRUCT(rounded_cylinder_props,
+       float radius;
+       float height;
+       float roundness;)
+
+STRUCT(solid_angle_props,
+       float angle;
+       float radius;)
+
+STRUCT(line_props,
+       vec3s start;
+       vec3s end;
+       float radius;)
+
+STRUCT(rounded_cone_props,
+       float radiusTop;
+       float radiusBottom;
+       float height;)
+
+STRUCT(vertical_capsule_props,
+       float radius;
+       float height;)
+
+STRUCT(capped_cone_props,
+       float radiusTop;
+       float radiusBottom;
+       float height;)
+
+STRUCT(capped_torus_props,
+       float radiusMajor;
+       float radiusMinor;)
+
+STRUCT(capped_cylinder_props,
+       float radius;
+       float height;)
+
+STRUCT(capped_plane_props,
+       vec2s dimensions;
+       float radius;)
+
 STRUCT(
     SDF_Primitive,
     SDF_PrimitiveType type;
     Transform         transform;    // Position, Rotation, Scale
     SDF_Material      material;
     union {
-        sphere_props    sphere;
-        box_props       box;
-        round_box_props round_box;
-        box_frame_props box_frame;
-
-        // Add more shapes as needed.
+        sphere_props           sphere;
+        box_props              box;
+        round_box_props        round_box;
+        box_frame_props        box_frame;
+        torus_props            torus;
+        torus_capped_props     torus_capped;
+        capsule_props          capsule;
+        cylinder_props         cylinder;
+        ellipsoid_props        ellipsoid;
+        quad_props             quad;
+        hexagonal_prism_props  hexagonal_prism;
+        triangular_prism_props triangular_prism;
+        cone_props             cone;
+        cone_section_props     cone_section;
+        plane_props            plane;
+        rounded_cylinder_props rounded_cylinder;
+        solid_angle_props      solid_angle;
+        line_props             line;
+        rounded_cone_props     rounded_cone;
+        vertical_capsule_props vertical_capsule;
+        capped_cone_props      capped_cone;
+        capped_torus_props     capped_torus;
+        capped_cylinder_props  capped_cylinder;
+        capped_plane_props     capped_plane;
+        // Add more as needed
+        // Max of 8 floats are needed to pack all props in a flattened view for GPU, update this as props get larger
+        vec4s packed_props[2];
     } props;)
 
 // Blending -> these are blending method b/w two primitives (eg. smooth union, XOR, etc. )  ( again refer iq)
